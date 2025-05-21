fe49e8c7ee21ae7cd64bb2c9991b444a72ebc664
kawatan1927 <haruto.kawata@solxyz.co.jp>
2025-05-21 17:13:53 +0900

    テンプレートを使いこなす　サンプルプロジェクト追加

---
+++ spring-02-template/src/main/java/jp/co/solxyz/spring/template/Spring02TemplateApplication.java
@@ -0,0 +1,13 @@
+package jp.co.solxyz.spring.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring02TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring02TemplateApplication.class, args);
+	}
+
+}
---
+++ spring-02-template/src/main/java/jp/co/solxyz/spring/template/controller/MainController.java
@@ -0,0 +1,14 @@
+package jp.co.solxyz.spring.template.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController{
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("message","Hello Thymeleaf");
+        return "index";
+    }
+}
