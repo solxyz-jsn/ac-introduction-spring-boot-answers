42351d2259167ab2e8f68ac111ce4eb9578412e8
kawatan1927 <haruto.kawata@solxyz.co.jp>
2025-05-22 11:36:11 +0900

    タブ文字インデントになっていた個所の修正

--- spring-04-di/src/main/java/jp/co/solxyz/spring/di/withdi/WithDiApplication.java
+++ spring-04-di/src/main/java/jp/co/solxyz/spring/di/withdi/WithDiApplication.java
@@ -7,20 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WithDiApplication implements CommandLineRunner {
 
-	private final Company company;
+    private final Company company;
 
-	public WithDiApplication(Company company) {
-		this.company = company;
-	}
+    public WithDiApplication(Company company) {
+        this.company = company;
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(WithDiApplication.class, args);
-	}
+        SpringApplication.run(WithDiApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		company.run();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        company.run();
+    }
 
 }
