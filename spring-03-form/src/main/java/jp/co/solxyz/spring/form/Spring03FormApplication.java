77e9f8cab051ab3c2b498526db53b15ac421598e
kawatan1927 <haruto.kawata@solxyz.co.jp>
2025-05-22 17:00:57 +0900

    フォーマット

--- spring-03-form/src/main/java/jp/co/solxyz/spring/form/Spring03FormApplication.java
+++ spring-03-form/src/main/java/jp/co/solxyz/spring/form/Spring03FormApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring03FormApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring03FormApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring03FormApplication.class, args);
+    }
 
 }
--- spring-03-form/src/main/java/jp/co/solxyz/spring/form/dto/WebForm.java
+++ spring-03-form/src/main/java/jp/co/solxyz/spring/form/dto/WebForm.java
@@ -8,16 +8,22 @@ import lombok.Data;
 
 @Data
 public class WebForm {
-    /** 氏名 */
+    /**
+     * 氏名
+     */
     @NotEmpty(message = "氏名を入力してください")
     private String name;
 
-    /** 年齢 */
+    /**
+     * 年齢
+     */
     @NotNull(message = "年齢を入力してください")
-    @Min(value=0, message = "生後の年齢を入力してください")
+    @Min(value = 0, message = "生後の年齢を入力してください")
     private Integer age;
 
-    /** 郵便番号 */
+    /**
+     * 郵便番号
+     */
     @NotNull(message = "郵便番号を入力してください")
     @Pattern(regexp = "[0-9]{3}[-]?[0-9]{4}", message = "郵便番号の形式で入力してください")
     private String postal;
