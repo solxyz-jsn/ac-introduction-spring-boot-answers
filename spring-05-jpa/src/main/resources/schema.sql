5142c6a479ce7df76fec9e26ba6d2a2443fded77
kawatan1927 <haruto.kawata@solxyz.co.jp>
2025-05-22 15:08:50 +0900

    JPAによるDB操作 サンプルプロジェクト作成

---
+++ spring-05-jpa/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+-- テーブル作成
+CREATE TABLE t_emp (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(100) NOT NULL,
+                       position VARCHAR(100),
+                       age INT
+);
