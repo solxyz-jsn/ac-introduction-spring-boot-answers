dfd19d2a59313cafc99a58bba973c712f3c32df0
kawatan1927 <haruto.kawata@solxyz.co.jp>
2025-05-23 09:18:28 +0900

    フォーマット

--- spring-01-mvc/src/test/java/jp/co/solxyz/spring/mvc/Spring01MvcApplicationTests.java
+++ spring-01-mvc/src/test/java/jp/co/solxyz/spring/mvc/Spring01MvcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring01MvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
