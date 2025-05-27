594b390e8c7e94e3ef1373a7f5339becc4e228c3
kawatan1927 <haruto.kawata@solxyz.co.jp>
2025-05-27 10:22:01 +0900

    JavaDocの追加

--- spring-02-template/src/main/java/jp/co/solxyz/spring/template/Spring02TemplateApplication.java
+++ spring-02-template/src/main/java/jp/co/solxyz/spring/template/Spring02TemplateApplication.java
@@ -3,6 +3,9 @@ package jp.co.solxyz.spring.template;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Bootアプリケーションのメインクラス
+ */
 @SpringBootApplication
 public class Spring02TemplateApplication {
 
--- spring-02-template/src/main/java/jp/co/solxyz/spring/template/controller/MainController.java
+++ spring-02-template/src/main/java/jp/co/solxyz/spring/template/controller/MainController.java
@@ -4,8 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * メイン画面のコントローラークラス
+ */
 @Controller
 public class MainController {
+    /**
+     * ルートURL("/")へのリクエストを処理するメソッド
+     * 指定したメッセージをモデルに追加し、ビューとして"index"を返す。
+     *
+     * @param model モデルオブジェクト
+     * @return 表示するビューの名前
+     */
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("message", "Hello Thymeleaf");
